abc1f0de65def989c0aeb965b85f5b9ce3410ba9
Eric-H-Thomas <103595220+Eric-H-Thomas@users.noreply.github.com>
2024-07-31 08:50:08 -0600

    Add config files to GitIgnore; Fix warnings with parameter names shadowing variables from outside scope

--- main.py
+++ main.py
@@ -1,72 +1,73 @@
 import itertools
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 from shapely.geometry import Point
-from shapely.ops import cascaded_union
+from shapely.ops import unary_union
 
 
 # Function to create random circles
 def create_random_circles(n, radius_range, board_size):
-    circles = []
+    _circles = []
     for _ in range(n):
         radius = np.random.uniform(*radius_range)
         center = (np.random.uniform(radius, board_size[0] - radius),
                   np.random.uniform(radius, board_size[1] - radius))
-        circles.append((center, radius))
-    return circles
+        _circles.append((center, radius))
+    return _circles
 
 
 # Function to calculate the total covered area of given circles
-def total_covered_area(circles):
-    shapes = [Point(c[0]).buffer(c[1]) for c in circles]
-    union = cascaded_union(shapes)
+def total_covered_area(_circles):
+    shapes = [Point(c[0]).buffer(c[1]) for c in _circles]
+    union = unary_union(shapes)
     return union.area
 
 
 # Function to visualize the circles
-def plot_circles(ax, circles, chosen_indices=None):
-    ax.clear()
-    for idx, c in enumerate(circles):
+def plot_circles(_ax, _circles, chosen_indices=None):
+    _ax.clear()
+    for idx, c in enumerate(_circles):
         circle = plt.Circle(c[0], c[1], fill=True, alpha=0.5, edgecolor='black')
         if chosen_indices and idx in chosen_indices:
             circle.set_edgecolor('red')
             circle.set_linewidth(2)
-        ax.add_artist(circle)
-    ax.set_xlim(0, board_size[0])
-    ax.set_ylim(0, board_size[1])
-    ax.set_aspect('equal', adjustable='box')
+        _ax.add_artist(circle)
+    _ax.set_xlim(0, boardSize[0])
+    _ax.set_ylim(0, boardSize[1])
+    _ax.set_aspect('equal', adjustable='box')
     plt.draw()
 
 
 # Function to simulate the game
-def simulate_game(circles, player_choices):
-    chosen_circles = [circles[i] for i in player_choices]
+def simulate_game(_circles, player_choices):
+    chosen_circles = [_circles[i] for i in player_choices]
     social_welfare = total_covered_area(chosen_circles)
 
     # Calculate marginal contribution for each player
-    contributions = []
+    _contributions = []
     for i in range(len(player_choices)):
-        other_circles = [circles[j] for j in player_choices if j != player_choices[i]]
+        other_circles = [_circles[j] for j in player_choices if j != player_choices[i]]
         area_without = total_covered_area(other_circles)
-        area_with = total_covered_area(other_circles + [circles[player_choices[i]]])
-        contributions.append(area_with - area_without)
+        area_with = total_covered_area(other_circles + [_circles[player_choices[i]]])
+        _contributions.append(area_with - area_without)
 
-    return contributions, social_welfare
+    return _contributions, social_welfare
 
 
 # Function to calculate the Shapley value for each player
-def calculate_shapley_values(circles, player_choices):
+def calculate_shapley_values(_circles, player_choices):
     num_players = len(player_choices)
     shapley_values = np.zeros(num_players)
-    factorial = np.math.factorial
+    factorial = math.factorial
 
     for i in range(num_players):
         for coalition in itertools.chain.from_iterable(
                 itertools.combinations(range(num_players), r) for r in range(num_players + 1)):
             if i not in coalition:
                 coalition_with_i = list(coalition) + [i]
-                area_without = total_covered_area([circles[player_choices[j]] for j in coalition])
-                area_with = total_covered_area([circles[player_choices[j]] for j in coalition_with_i])
+                area_without = total_covered_area([_circles[player_choices[j]] for j in coalition])
+                area_with = total_covered_area([_circles[player_choices[j]] for j in coalition_with_i])
                 marginal_contribution = area_with - area_without
                 shapley_values[i] += marginal_contribution * (
                             factorial(len(coalition)) * factorial(num_players - len(coalition) - 1)) / factorial(
@@ -76,19 +77,19 @@ def calculate_shapley_values(circles, player_choices):
 
 
 # Function to calculate the optimal social welfare
-def calculate_optimal_social_welfare(circles):
-    return total_covered_area(circles)
+def calculate_optimal_social_welfare(_circles):
+    return total_covered_area(_circles)
 
 
 # Function to calculate the Price of Anarchy
-def calculate_price_of_anarchy(circles, player_choices):
-    _, social_welfare_marginal = simulate_game(circles, player_choices)
-    shapley_values = calculate_shapley_values(circles, player_choices)
+def calculate_price_of_anarchy(_circles, player_choices):
+    _, social_welfare_marginal = simulate_game(_circles, player_choices)
+    shapley_values = calculate_shapley_values(_circles, player_choices)
 
     sorted_indices_by_shapley = np.argsort(-shapley_values).tolist()
-    _, social_welfare_shapley = simulate_game(circles, sorted_indices_by_shapley)
+    _, social_welfare_shapley = simulate_game(_circles, sorted_indices_by_shapley)
 
-    optimal_social_welfare = calculate_optimal_social_welfare(circles)
+    optimal_social_welfare = calculate_optimal_social_welfare(_circles)
 
     poa_marginal = optimal_social_welfare / social_welfare_marginal
     poa_shapley = optimal_social_welfare / social_welfare_shapley
@@ -97,28 +98,28 @@ def calculate_price_of_anarchy(circles, player_choices):
 
 
 # Parameters
-num_circles = 6
-radius_range = (1, 3)
-board_size = (10, 10)
+numCircles = 6
+radiusRange = (1, 3)
+boardSize = (10, 10)
 
 # Create random circles
-circles = create_random_circles(num_circles, radius_range, board_size)
+circles = create_random_circles(numCircles, radiusRange, boardSize)
 
 # Interactive part
-chosen_indices = []
+chosenIndices = []
 
 
 def onclick(event):
-    global chosen_indices
+    global chosenIndices
     for i, c in enumerate(circles):
         distance = np.sqrt((c[0][0] - event.xdata) ** 2 + (c[0][1] - event.ydata) ** 2)
         if distance <= c[1]:
-            if i not in chosen_indices:
-                chosen_indices.append(i)
+            if i not in chosenIndices:
+                chosenIndices.append(i)
             else:
-                chosen_indices.remove(i)
+                chosenIndices.remove(i)
             break
-    plot_circles(ax, circles, chosen_indices)
+    plot_circles(ax, circles, chosenIndices)
 
 
 # Plot the circles and set up the event handler
@@ -131,17 +132,17 @@ cid = fig.canvas.mpl_connect('button_press_event', onclick)
 plt.show()
 
 # Simulate the game after selection is complete
-contributions, social_welfare = simulate_game(circles, chosen_indices)
+contributions, socialWelfare = simulate_game(circles, chosenIndices)
 
 # Calculate Shapley values
-shapley_values = calculate_shapley_values(circles, chosen_indices)
+shapleyValues = calculate_shapley_values(circles, chosenIndices)
 
 # Calculate Price of Anarchy
-poa_marginal, poa_shapley = calculate_price_of_anarchy(circles, chosen_indices)
+poaMarginal, poaShapley = calculate_price_of_anarchy(circles, chosenIndices)
 
 # Display results
 print("Player Contributions:", contributions)
-print("Social Welfare:", social_welfare)
-print("Shapley Values:", shapley_values)
-print("Price of Anarchy (Marginal):", poa_marginal)
-print("Price of Anarchy (Shapley):", poa_shapley)
+print("Social Welfare:", socialWelfare)
+print("Shapley Values:", shapleyValues)
+print("Price of Anarchy (Marginal):", poaMarginal)
+print("Price of Anarchy (Shapley):", poaShapley)
